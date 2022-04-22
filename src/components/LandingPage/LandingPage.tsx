bf5dfbedf53ccda3de26b427c70b34e71da8433e
Adeniyii <ifedayoadeniyi@gmail.com>
2022-04-22 19:05:28 +0100

    fix

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -5,20 +5,20 @@ function LandingPage() {
   return (
     <div className="max-w-screen-lg mx-auto">
       <section className="min-h-screen pt-40">
-        <h1 className="text-7xl font-bold">Landing Page Boii</h1>
+        <h1 className="text-6xl font-bold">Landing Page</h1>
         <div className="mt-16 flex gap-3">
           <div className="inline-flex rounded shadow">
             <Link
               to="/dashboard"
-              className="grid place-content-center px-5 py-3 border border-transparent text-base font-medium rounded text-white bg-indigo-600 hover:bg-indigo-700"
+              className="grid place-content-center px-5 py-3 border border-transparent text-base font-medium rounded text-white bg-gray-600 hover:bg-gray-700"
             >
-              Get started
+              Dashboard
             </Link>
           </div>
           <div className="inline-flex rounded shadow">
             <Link
               to="/"
-              className="grid place-content-center px-5 py-3 border border-transparent text-base font-medium rounded text-indigo-600 bg-white hover:bg-indigo-50"
+              className="grid place-content-center px-5 py-3 border border-transparent text-base font-medium rounded text-gray-600 bg-white hover:bg-gray-50"
             >
               Learn more
             </Link>
